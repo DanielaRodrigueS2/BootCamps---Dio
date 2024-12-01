3efee2c5b8131c36271354adf2d8887f477fdd59
Daniela <danielarodrigues2po@gmail.com>
2024-12-01 20:25:19 -0300

    BancoV2 - definição das funções

---
+++ Python/BancoV2.py
@@ -0,0 +1,29 @@
+users = [{}] # nome, data de nascimento, cpf e endereço
+contas = [{}] #agencia, numero da conta e usuário, num de conta é sequencial (inicia em 1) e agencia é fica em 0001
+
+def saque(saldo, valor, extrato, limite, numero_saques, limite_saques):
+    return saldo, extrato
+
+def deposito(saldo, valor, extrato, /):
+
+    return saldo, extrato
+
+def extrato(saldo, /, extrato):
+    return 
+
+def criarUser():
+    return
+
+def criarConta():
+    return
+
+def isFloat(numero):
+    try:
+        float(numero)
+        return True
+    
+    except:
+        return False
+
+def main():
+    return
