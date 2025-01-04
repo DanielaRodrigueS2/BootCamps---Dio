2c0e1a5d3272409e2035ebed1f4e8cf8355047e3
Daniela <danielarodrigues2po@gmail.com>
2025-01-03 21:15:50 -0300

    Implementação primeiras funções e correções do diagrama de classes

--- Python/BancoV3 (POO).py
+++ Python/BancoV3 (POO).py
@@ -4,22 +4,22 @@ user_logado = None
 
 class Conta:
 
-    def __init__(self, cliente, historico):
+    def __init__(self, cliente):
         self._cliente = cliente
         self._saldo = 0.0
         self._agencia = '0001'
         self._numero = len(cliente.contas) + 1
-        self._historico = historico
+        self._historico = Historico()
 
-    def novaConta(cliente, numero):
+    def novaConta(self,cliente, numero):
         conta = Conta(cliente, numero)
         return conta
 
-    def sacar(valor):
+    def sacar(self,valor):
         pass
 
-    def depositar(valor):
-        pass
+    def depositar(self,valor):
+        self._saldo += valor
 
     @property
     def saldo(self):
@@ -33,7 +33,11 @@ class ContaCorrente(Conta):
 
 class Historico:
 
-    def adicionar_transacao(transacao):
+    def __init__(self):
+        self._transacoes = []
+
+    def adicionar_transacao(self,transacao):
+        self._transacoes.append(transacao)
         pass
 
 class Transacao(ABC):
@@ -47,8 +51,18 @@ class Deposito(Transacao):
     def __init__(self, valor):
         self.valor = valor
 
-    def registrar(conta):
-        return super().registrar()
+    def registrar(self, conta):
+        valor = input('\nDigite o valor que deseja depositar: ')
+    
+        if isFloat(self.valor):
+            valor = float(self.valor)
+            if valor > 0:
+                conta.depositar(valor)
+                conta._historico.adicionar_transacao(self)
+            else:
+                print('\nValor inválido.')
+        else:
+            print('\nValor inválido.')
     
 class Saque(Transacao):
 
@@ -61,22 +75,35 @@ class Saque(Transacao):
 class PessoaFisica:
 
     def __init__(self, cpf, nome, data_nascimento):
-        self.cpf = cpf
-        self.nome = nome
-        self.data_nascimento = data_nascimento
+        self._cpf = cpf
+        self._nome = nome
+        self._data_nascimento = data_nascimento
 
 class Cliente(PessoaFisica):
 
-    def __init__(self, cpf, nome, data_nascimento, endereco, contas):
+    def __init__(self, cpf, nome, data_nascimento, endereco, contas,senha):
         super().__init__(cpf, nome, data_nascimento)
-        self.endereco = endereco
-        self.contas = contas
+        self._endereco = endereco
+        self._contas = contas
+        self._senha = senha
 
     def realizar_transacao(conta, transacao):
         pass
 
     def adicionar_conta(conta):
         pass
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def senha(self):
+        return self._senha
+    
+    @property
+    def contas(self):
+        return self._contas
 
 def main():
     #implementação de operacoes
@@ -127,8 +154,89 @@ def textoMenu(tipo):
 
 
 def login():
-    pass
+    global user_logado
+    print('\nInserir CPF:')
+    cpf = input('\nDigite seu cpf (sem caracteres especiais): ')
+    user = buscaCpf(cpf)
+    if user is None:
+        print('\nCPF inválido')
+        return False
+    senha = input('\nDigite a senha: ')
+    if(senha == user.senha):
+        print('\nLogin realizado com sucesso!')
+        user_logado = user
+        menu_login(user, True)
+        return True
+    else:
+        print('\nSenha incorreta!')
+        return False
 
 def criarUser():
-    pass
+    print('\nMenu de criação de usuário: ')
+    nome = input('\nDigite seu nome: ')
+    data = input('\nDigite sua data de nascimento: ')
+    cpf = input('\nDigite seu cpf (sem caracteres especiais): ')
+    endereco = input('\nDigite seu endereço: ')
+    senha = input('Digite uma senha: ')
+    if len(cpf) != 11:
+        print('\nCPF inválido!')
+        return 
+    if buscaCpf(cpf) != None: 
+        print('\nCPF já cadastrado!') 
+        return
+    contas = []
+    user = Cliente(cpf, nome, data,endereco, contas, senha)
+    users.append(user)
+    print(users)
+    return
+    
+def buscaCpf(cpf):
+    if users == []:
+        return None
+    
+    for user in users:
+        if(user.cpf == cpf):
+            return user
+    
+    return None
 
+def menu_login(user, login):
+    while(login):
+        op = textoMenu(2)
+        match(op):
+            case '1':
+                num_conta = input('\nDigite o número da conta: ')
+                num_conta = buscaConta(user, num_conta)
+                if(num_conta != None):
+                    saque(user, num_conta)
+                
+            case '2':
+                num_conta = input('\nDigite o número da conta: ')
+                num_conta = buscaConta(user, num_conta)
+                if(num_conta != None):
+                    deposito(user, num_conta)
+                
+            case '3':
+                extrato(user)
+
+            case '4':
+                criarConta()
+
+            case '5':
+                print(users[user])
+
+            case '6':
+                login = False    
+
+            case _:
+                print('\nOpção inválida')
+
+def isFloat(numero):
+    try:
+        float(numero)
+        return True
+    
+    except:
+        return False
+
+main()
