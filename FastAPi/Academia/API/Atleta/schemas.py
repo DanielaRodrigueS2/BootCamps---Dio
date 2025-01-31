56d70426dad9325f95b31bce6c4bc92ee8b70103
Daniela <danielarodrigues2po@gmail.com>
2025-01-31 14:32:41 -0300

    Api com fastAPI

---
+++ FastAPi/Academia/API/Atleta/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field, PositiveFloat
+from typing import Annotated
+
+class Atleta(BaseModel):
+    nome : Annotated[str, Field(description='Nome da/do Atleta', examples='Dani', max_length=70)]
+    cpf : Annotated[str, Field(description='CPF da/do Atleta', examples='12312312312', max_length=11)]
+    idade : Annotated[int, Field(description='Idaded da/do Atleta', examples=21)]
+    peso : Annotated[PositiveFloat, Field(description='Peso da/do Atleta', examples=60.5)]
+    altura : Annotated[PositiveFloat, Field(description='Altura da/do Atleta', examples=1.72)]
+    genero : Annotated[str, Field(description='Genero da/do Atleta', examples='Feminino', max_length=20)]
